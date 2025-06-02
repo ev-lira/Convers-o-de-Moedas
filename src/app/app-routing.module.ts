16e1495b6f24a8791d2b5dc2b110a08dad8ae74b
Vítor Lira <erlon2024.vtr@gmail.com>
2025-06-01 22:14:07 -0300

    Add files via upload

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  {
+    path: '',
+    loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
+  },
+  {
+    path: 'currency-selector-modal',
+    loadChildren: () => import('./components/currency-selector-modal/currency-selector-modal.module').then( m => m.CurrencySelectorModalPageModule)
+  }
+];
+
+@NgModule({
+  imports: [
+    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
+  ],
+  exports: [RouterModule]
+})
+export class AppRoutingModule {}
